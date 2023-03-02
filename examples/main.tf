6f42bc95401ef145d9b2275ce388c161eac962b6
Hasindu Malalaarachchi <hasindumalalaarachchi@Hasindus-MacBook-Air.local>
2023-03-02 19:15:27 +0200

    revert example changes

--- examples/main.tf
+++ examples/main.tf
@@ -7,63 +7,61 @@ terraform {
   }
 }
 
-
-# resource "ametnes_service" "network" {
-#   name = "NETWORK-EUW2"
-#   project_name = "Demo"
-#   location = "EUW1"
-#   kind = "network/loadbalancer:1.0"
-#   description = "My name is Sample"
-#   storage = 1
-#   memory = 1
-#   nodes = 1
-#   cpu = 1
-# }
-
-resource "ametnes_network" "network" {
-  name = "NETWORK-EUW3"
-  project_name = "Demo"
-  location = "EUW1"
-  description = "My network resouce"
+provider "ametnes" {
+  // add you provider here
+  host = "https://api-test.cloud.ametnes.com/v1"
+  insecure = true
 }
 
-# resource "ametnes_service" "mysql" {
-#   count = 3
-#   name = "Test Sample ${count.index}"
-#   project_name = "Default"
-#   location = "aws/eu-west-2"
-#   kind = "service/mysql:8.0"
-#   description = "My name is Sample"
-#   network = tonumber(element(split("/", ametnes_service.network.id), 1))
-#   storage = 1
-#   memory = 1
-#   nodes = 1
-#   cpu = 1
-# }
+resource "ametnes_service" "network" {
+  name = "NETWORK-EUW2"
+  project_name = "Default"
+  location = "aws/eu-west-2"
+  kind = "network/privatelink:1.0"
+  description = "My name is Sample"
+  storage = 1
+  memory = 1
+  nodes = 1
+  cpu = 1
+}
 
-# resource "ametnes_service" "postgres" {
-#   count = 4
-#   name = "MySql Db ${count.index}"
-#   project_name = "Default"
-#   location = "aws/eu-west-2"
-#   kind = "service/mysql:8.0"
-#   description = "My name is Sample"
-#   network = tonumber(element(split("/", ametnes_service.network.id), 1))
-#   storage = 1
-#   memory = 1
-#   nodes = 1
-#   cpu = 1
-# }
+resource "ametnes_service" "mysql" {
+  count = 3
+  name = "Test Sample ${count.index}"
+  project_name = "Default"
+  location = "aws/eu-west-2"
+  kind = "service/mysql:8.0"
+  description = "My name is Sample"
+  network = tonumber(element(split("/", ametnes_service.network.id), 1))
+  storage = 1
+  memory = 1
+  nodes = 1
+  cpu = 1
+}
 
-# resource "ametnes_service" "pg" {
-#   count = 4
-#   name = "Postgres DB ${count.index}"
-#   project_name = "Default"
-#   location = "gcp/europe-west2"
-#   kind = "service/postgres:11.9"
-#   description = "My name is Sample"
-#   storage = 1
-#   memory = 1
-#   nodes = 1
-#   cpu = 1
-# }
+resource "ametnes_service" "postgres" {
+  count = 4
+  name = "MySql Db ${count.index}"
+  project_name = "Default"
+  location = "aws/eu-west-2"
+  kind = "service/mysql:8.0"
+  description = "My name is Sample"
+  network = tonumber(element(split("/", ametnes_service.network.id), 1))
+  storage = 1
+  memory = 1
+  nodes = 1
+  cpu = 1
+}
+
+resource "ametnes_service" "pg" {
+  count = 4
+  name = "Postgres DB ${count.index}"
+  project_name = "Default"
+  location = "gcp/europe-west2"
+  kind = "service/postgres:11.9"
+  description = "My name is Sample"
+  storage = 1
+  memory = 1
+  nodes = 1
+  cpu = 1
+}
--- examples/provider.tf
+++
@@ -1,7 +0,0 @@
-provider "ametnes" {
-  // add you provider here
-  host = "https://api-test.cloud.ametnes.com/v1"
-  insecure = true
-  token = "cwa6X4dd9weqP342c66c67&@$t98=5P8aoe;L1tGdbab60A7zr05al4d"
-  username = "Brave.Microphone@ametnes.com"
-}
