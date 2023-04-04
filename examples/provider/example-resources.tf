7dbb57c2c7f0d7a473d6213df35110b3cecdcfde
Michael Sekamanya <Michael@Sekamanya@ametnes.com>
2023-04-04 11:07:18 +0100

    Separate provider examples

--- examples/provider/example-resources.tf
+++ examples/provider/example-resources.tf
@@ -1,39 +1,27 @@
-terraform {
-  required_providers {
-    ametnes = {
-      source  = "ametnes.com/cloud/ametnes"
-    }
-  }
-}
-
-provider "ametnes" {
-  // add you provider here
-  host = "https://cloud.ametnes.com/api/c/v1"
-  token = var.token
-  username = var.username
-
-}
-
-resource "ametnes_project" "project" {
-  name = "Demo"
-}
-
-resource "ametnes_location" "location" {
+# Create a lcoation.
+data "ametnes_location" "location" {
   name = "Ametnes Cloud"
   code = "EUW1"
 }
 
+# Create project that will host all your resources.
+resource "ametnes_project" "project" {
+  name = "Demo"
+}
+
+# Create a network access resource.
 resource "ametnes_network" "network" {
   name = "NETWORK-EUW5"
   project = ametnes_project.project.id
-  location = ametnes_location.location.id
+  location = data.ametnes_location.location.id
   description = "My loadbalance resource"
 }
 
+# Create a service resource.
 resource "ametnes_service" "mysql" {
   name = "MySql-Demo-Instance"
   project = ametnes_project.project.id
-  location = ametnes_location.location.id
+  location = data.ametnes_location.location.id
   kind = "mysql:8.0"
   description = "Mysql Demo Instance"
   network = ametnes_network.network.resource_id
